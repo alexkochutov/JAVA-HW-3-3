e8cf7946f3c379b181a65af6ac62797119b7a10b
alexkochutov <alex.kochutov@gmail.com>
2022-02-07 11:50:21 +0300

    Added Channel control methods and tests

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -1,9 +1,60 @@
 package ru.netology.domain;
 
 public class Radio {
+    private int firstChannel = 0;
+    private int countChannel = 10;
+    private int lastChannel = 9;
+    private int currentChannel;
     private int minVolume = 0;
     private int maxVolume = 100;
     private int currentVolume;
+
+    public Radio() {
+    }
+
+    public Radio(int countChannel) {
+        this.countChannel = countChannel;
+        this.lastChannel = countChannel - 1;
+    }
+
+    public int getFirstChannel() {
+        return firstChannel;
+    }
+
+    public int getCountChannel() {
+        return countChannel;
+    }
+
+    public int getLastChannel() {
+        return lastChannel;
+    }
+
+    public int getCurrentChannel() {
+        return currentChannel;
+    }
+
+    public void setCurrentChannel(int currentChannel) {
+        if ((currentChannel < this.getFirstChannel()) || (currentChannel > this.getLastChannel())) {
+            return;
+        }
+        this.currentChannel = currentChannel;
+    }
+
+    public void nextChannel() {
+        if (currentChannel == getLastChannel()) {
+            currentChannel = getFirstChannel();
+            return;
+        }
+        setCurrentChannel(++currentChannel);
+    }
+
+    public void prevChannel() {
+        if (currentChannel == getFirstChannel()) {
+            currentChannel = getLastChannel();
+            return;
+        }
+        setCurrentChannel(--currentChannel);
+    }
 
     public int getMinVolume() {
         return minVolume;
--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -11,6 +11,103 @@ class RadioTest {
     @CsvSource(
             value = {
                     "shouldNotBeNegativeValue, -1, 0",
+                    "shouldBeEqualFirstChannel, 0, 0",
+                    "shouldBePositiveValue, 1, 1",
+                    "shouldBeLessThenLastChannel, 8, 8",
+                    "shouldBeEqualLastChannel, 9, 9",
+                    "shouldNotBeMoreThenMaxVolume, 10, 0"
+            }, delimiter = ',')
+    void shouldSetCurrentChannel(String testName, int currentChannel, int expected) {
+        Radio radio = new Radio();
+
+        radio.setCurrentChannel(currentChannel);
+
+        assertEquals(expected, radio.getCurrentChannel());
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    "shouldIncreaseCurrentChannel, 8, 9",
+                    "shouldSetToFirstChannel, 9, 0"
+            }, delimiter = ',')
+    void shouldChangeNextStation(String testName, int currentChannel, int expected) {
+        Radio radio = new Radio();
+
+        radio.setCurrentChannel(currentChannel);
+        radio.nextChannel();
+
+        assertEquals(expected, radio.getCurrentChannel());
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    "shouldDecreaseCurrentChannel, 1, 0",
+                    "shouldSetToFirstChannel, 0, 9"
+            }, delimiter = ',')
+    void shouldChangePrevStation(String testName, int currentChannel, int expected) {
+        Radio radio = new Radio();
+
+        radio.setCurrentChannel(currentChannel);
+        radio.prevChannel();
+
+        assertEquals(expected, radio.getCurrentChannel());
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    "shouldNotBeNegativeValue, -1, 0",
+                    "shouldBeEqualFirstChannel, 0, 0",
+                    "shouldBePositiveValue, 1, 1",
+                    "shouldBeLessThenLastChannel, 98, 98",
+                    "shouldBeEqualLastChannel, 99, 99",
+                    "shouldNotBeMoreThenMaxVolume, 100, 0"
+            }, delimiter = ',')
+    void shouldSetCurrentChannelForCustomConstructor(String testName, int currentChannel, int expected) {
+        Radio radio = new Radio(100);
+
+        radio.setCurrentChannel(currentChannel);
+
+        assertEquals(expected, radio.getCurrentChannel());
+    }
+
+
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    "shouldIncreaseCurrentChannel, 98, 99",
+                    "shouldSetToFirstChannel, 99, 0"
+            }, delimiter = ',')
+    void shouldChangeNextStationForCustomConstructor(String testName, int currentChannel, int expected) {
+        Radio radio = new Radio(100);
+
+        radio.setCurrentChannel(currentChannel);
+        radio.nextChannel();
+
+        assertEquals(expected, radio.getCurrentChannel());
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    "shouldDecreaseCurrentChannel, 1, 0",
+                    "shouldSetToFirstChannel, 0, 99"
+            }, delimiter = ',')
+    void shouldChangePrevStationForCustomConstructor(String testName, int currentChannel, int expected) {
+        Radio radio = new Radio(100);
+
+        radio.setCurrentChannel(currentChannel);
+        radio.prevChannel();
+
+        assertEquals(expected, radio.getCurrentChannel());
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    "shouldNotBeNegativeValue, -1, 0",
                     "shouldBeEqualZero, 0, 0",
                     "shouldBePositiveValue, 1, 1",
                     "shouldBeLessThenMaxVolume, 99, 99",
